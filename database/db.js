eeed6ea3588ef82c7f1ae27e4068daf614e95f97
birari2003 <gauravbirari07@gmail.com>
2025-07-17 09:27:25 +0530

    connect db to railway

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({ 
-  host: 'sql305.byethost18.com',
-  user: '	b18_22538669',
-  password: 'yuvraj7600', 
-  database: 'b18_22538669_milk_mate'
+  host: 'mysql.railway.internal',
+  user: 'root',
+  password: 'rFtbKLajkbEoOieiQdNvebNSNcXcjbyd', 
+  database: 'railway'
 });
 
 db.connect(err => {
